1f91f2de49761d7cb4bb1d0c1ce571dd70264456
JonasSvegland <jonas@svegland.se>
2024-11-05 11:50:41 +0100

    Inititial commit

---
+++ Runtime/AudioCollection.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WxTools.Audio
+{
+    [Serializable]
+    public class AudioObject
+    {
+        [SerializeField]
+        private string name;
+
+        [SerializeField]
+        private AudioClip clip;
+        [SerializeField]
+        private float minVolume = 1.0f;
+        [SerializeField]
+        private float maxVolume = 1.0f;
+        [SerializeField]
+        private float minPitch = 1.0f;
+        [SerializeField]
+        private float maxPitch = 1.0f;
+
+
+        public AudioClip Clip { get => clip; set => clip = value; }
+        public float MinPitch { get => minPitch; set => minPitch = value; }
+        public float MaxPitch { get => maxPitch; set => maxPitch = value; }
+        public float MinVolume { get => minVolume; set => minVolume = value; }
+        public float MaxVolume { get => maxVolume; set => maxVolume = value; }
+        public string Name { get => name; set => name = value; }
+    }
+
+
+    [CreateAssetMenu(fileName = "RandomAudioCollection", menuName = "RandomAudio/AudioCollection", order = 1)]
+    public class AudioCollection : ScriptableObject
+    {
+        public List<AudioObject> audioObjects;
+
+
+        public void OnValidate()
+        {
+            foreach (AudioObject obj in audioObjects)
+            {
+                if (obj.Clip != null)
+                {
+                    string name = obj.Clip.name;
+                    name = name.Trim();
+                    name = name.Replace('_', ' ');
+                    obj.Name = name;
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/RandomAudioPlayer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace WxTools.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class RandomAudioPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayOneShot(AudioCollection collection)
+        {
+            int index = Random.Range(0, collection.audioObjects.Count);
+            AudioObject audio = collection.audioObjects[index];
+            if (audio == null)
+            {
+                Debug.LogError("No AudioObject at " + index);
+                return;
+            }
+
+            if (audio.Clip == null)
+            {
+                Debug.LogError("No AudioClip in " + audio.ToString());
+                return;
+            }
+
+            audioSource.pitch = Random.Range(audio.MinPitch, audio.MaxPitch);
+            audioSource.volume = Random.Range(audio.MinVolume, audio.MaxVolume);
+            audioSource.PlayOneShot(audio.Clip);
+
+        }
+    }
+}
